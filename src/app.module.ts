89bc165eebab516b8b4c45aaccacae1dba8e05bd
Flash Lavon <flash.lavon@hotmail.com>
2025-05-08 12:39:54 +0700

    log: fixing materi yg tertinggal

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,16 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ValidationModule } from './validation/validation.module';
 import { LogMiddleware } from './log/log.middleware';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      level: 'debug',
+      transports: [new winston.transports.Console()],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -24,6 +31,11 @@ import { LogMiddleware } from './log/log.middleware';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule{
-
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes({
+      path: '/api/*',
+      method: RequestMethod.ALL,
+    });
+  }
 }
