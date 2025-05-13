3a65a1a79a71d837dbbcd68856cd1caec35917ba
flashlf <flash.lavon@hotmail.com>
2025-05-13 13:22:00 +0700

    guard: penggunaan global guard scope aplikasi

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ValidationFilter } from './validation/validation.filter';
+import { RoleGuard } from './role/role.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,6 +19,7 @@ async function bootstrap() {
   app.set('view engine', 'html');
   app.engine('html', mustache());
   app.useGlobalFilters(new ValidationFilter());
+  // app.useGlobalGuards(new RoleGuard(['admin', 'user']));
   await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
