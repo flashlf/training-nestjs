d953ad1bcdd8e31356cbcece31dca4bf38b7e423
flashlf <flash.lavon@hotmail.com>
2025-05-13 13:58:08 +0700

    reflector: contoh pengunaan reflector untuk menerapkan singleton pada decorator Guard, agar tidak terjadi penggunaan memory secara excesive

--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -2,7 +2,10 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super();
     console.info('Create Prisma service');
--- src/role/role.guard.ts
+++ src/role/role.guard.ts
@@ -1,12 +1,20 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { Roles } from './role.decorator';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  constructor(private roles: string[]) {}
+  constructor(private relfector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles: string[] = this.relfector.get(Roles, context.getHandler());
+
+    if (!roles) {
+      return true;
+    }
+
     const user = context.switchToHttp().getRequest().user;
     return this.roles.indexOf(user.role) !== -1;
   }
